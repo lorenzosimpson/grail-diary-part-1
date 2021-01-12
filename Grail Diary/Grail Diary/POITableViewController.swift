8b58408a2e7274a461621e0ee522c662f2acecae
Lorenzo Simpson <lorenzosimp@gmail.com>
2021-01-12 16:40:17 -0500

    image and location details aligned

---
+++ Grail Diary/Grail Diary/POITableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Grail Diary
+//
+//  Created by Lorenzo on 1/12/21.
+//
+
+import UIKit
+
+class POITableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
